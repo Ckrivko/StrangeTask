7a04b8502b1bd3476a9f5117bb63125403457966
Ckrivko <76474570+Ckrivko@users.noreply.github.com>
2021-02-26 15:11:14 +0200

    Add files via upload

---
+++ StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Elf elf = new Elf("Marina", 10);
+            Console.WriteLine(elf);
+
+
+        }
+    }
+}
